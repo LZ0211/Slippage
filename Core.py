ecffe22b118d344d681013addb20a1c1b3b1fccc
Liezhang0211 <1491097321@qq.com>
2020-03-13 19:19:40 +0800

    update
    change module structure

--- Core.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from lib.Engine import Engine
-from lib.DataSet import DataSet
-from lib.File import File
-from lib.Fitting import Fitting
-from lib.Smooth import Smooth
---
+++ Core/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+from .Engine import Engine
+from .DataSet import DataSet
+from .File import File
+from .Fitting import Fitting
+from .Smooth import Smooth
